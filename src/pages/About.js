caadd455cbc253a62230b5cbe88f5b77aa460455
Nathan Akpawu <nakpawu@gmail.com>
2023-02-08 08:30:47 +0000

    router and navigation

---
+++ src/pages/About.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Navigation />
+            <h1>A propos</h1>
+            <br />
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur facere aperiam
+                in, eius tempora nostrum et non impedit praesentium quaerat quasi iusto enim illo
+                saepe. Numquam minima qui tempora eveniet reiciendis harum, voluptate quo est, fugit
+                possimus ipsam voluptates suscipit alias deleniti officiis quos. Quis atque eaque
+                veniam, nobis autem hic perspiciatis ducimus voluptatibus. Unde, numquam ad quod
+                dicta vero suscipit rerum iure laboriosam eaque accusantium cum nesciunt porro,
+                distinctio consequatur nam obcaecati totam modi officia eveniet ipsam natus, nostrum
+                quo dolor facere? Harum voluptatibus molestiae veniam fuga sunt aut, obcaecati
+                distinctio, eligendi animi eum minus consectetur error possimus dolorum!
+            </p>
+            <br />
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium quos obcaecati
+                excepturi facilis similique distinctio doloribus impedit! Aliquid, sint a. Sapiente
+                aliquid sed aut. Ut temporibus dolor vitae quas explicabo a fugit quibusdam soluta
+                neque eius, ipsa, aperiam, labore aliquid!
+            </p>
+        </div>
+    );
+};
+
+export default About;
